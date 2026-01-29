73af4cec41754dd85eb0d8617c93cfb568292d02
MohamedHamdy <midohamdi487@gmail.com>
2026-01-30 00:26:20 +0200

    add ExecutionType to control type of execution

--- src/main/java/com/demoblaze/drivers/DriverFactory.java
+++ src/main/java/com/demoblaze/drivers/DriverFactory.java
@@ -1,17 +1,38 @@
 package com.demoblaze.drivers;
+import com.demoblaze.datareader.PropertyReader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 public class DriverFactory {
 
     public static WebDriver createDriver(String browser) {
+        WebDriver driver;
 
-        WebDriver driver = switch (browser.toLowerCase()) {
-            case "edge" -> new EdgeDriver();
-            case "chrome" -> new ChromeDriver();
+        String executionType = PropertyReader.getProperty("executionType");
+        switch (browser.toLowerCase()) {
+            case "chrome" -> {
+                ChromeOptions chromeOptions = new ChromeOptions();
+                if ("LocalHeadless".equalsIgnoreCase(executionType)) {
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--disable-gpu");
+                }
+                driver = new ChromeDriver(chromeOptions);
+            }
+
+            case "edge" -> {
+                EdgeOptions edgeOptions = new EdgeOptions();
+                if ("LocalHeadless".equalsIgnoreCase(executionType)) {
+                    edgeOptions.addArguments("--headless=new");
+                    edgeOptions.addArguments("--disable-gpu");
+                }
+                driver = new EdgeDriver(edgeOptions);
+            }
+
             default -> throw new IllegalArgumentException("Unsupported browser: " + browser);
-        };
+        }
 
         driver.manage().window().maximize();
         return driver;
